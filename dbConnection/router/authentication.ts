311b7b83dd6c60127ea315a853bf5900f8ba8c71
Franco <francolobos3@gmail.com>
2024-01-25 00:31:56 +0100

    MERGE

--- dbConnection/router/authentication.ts
+++ dbConnection/router/authentication.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {login, register, googleLogin, googleAuthenticate} from "../controllers/authentication";
 
 export default (router: express.Router) => {
+    
     router.post('/auth/google/authenticate', googleAuthenticate);
     router.post('/auth/google/login', googleLogin);
     router.post('/auth/register', register);
